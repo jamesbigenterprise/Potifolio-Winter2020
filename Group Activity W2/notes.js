d681a535ce6e6e694a2435ad2f06e2184f70aacd
Thiago Alves <thiagoalvesda0silva@gmail.com>
2020-01-21 14:13:32 -0700

    Week 3 Tuesday

--- Group Activity W2/notes.js
+++ Group Activity W2/notes.js
@@ -1,3 +1,5 @@
+//Notes
+
 // Arrays are special objects to store data
 const emptyArray = new Array();
 emptyArray[1] = "second item";
@@ -5,7 +7,7 @@ emptyArray[0] = "first item";
 emptyArray[2] = "third item";
 
 //the array destruction can be handy for quick swaps
-const [a,b] = [1,2];
+let [a,b] = [1,2];
 [a,b] = [2,1];
 
 //I wonder if this can be done with varibles created otherwise
@@ -22,7 +24,7 @@ const lehiSons = ["Laman", "Lemuel", "Sam", "Nephi"];
 lehiSons.splice(2,0, 'Joseph'); // Added Joseph right next to Sam
 
 //Removing content
-lehiSons.splice(0,2, Jacob);//We removed 2 items starting on zero
+lehiSons.splice(0,2, 'Jacob');//We removed 2 items starting on zero
 //Laman and Lemuel will not receive the same blessings as their brethren
 
 //We can use includes to check if an item is in the array
@@ -30,7 +32,7 @@ const LemuenInheritance = lehiSons.includes("Lemuel"); //it will not be in the a
 //sure enough, Lemuel lost his inheritance
 
 //multiDimentional arrays can be helpful to store data with several levels
-const apartment4 = [[Nathan, David, Ben, Ran, James], [Lamoni, Jack, Drake], [Moroni, Sam, Edward, Jacob]];
+const apartment4 = [['Nathan', 'David', 'Ben', 'Ran', 'James'], ['Lamoni', 'Jack', 'Drake'], ['Moroni', 'Sam', 'Edward', 'Jacob']];
 
 //If the data was inputed sequecially you could find the first tenant of the first apartment of this building
 let fistsTennant = apartment4[0][0];
@@ -39,3 +41,34 @@ let fistsTennant = apartment4[0][0];
 const allTennants = [...apartment4];     
 
 
+//A new thing to me was that we cant add several strings at once as you create the set
+//this would store each character separately
+let letters = new Set('world');
+//Using the add we can add strings
+let words = new Set().add('hello').add('world');
+//another interesting use for the spread operator
+const wordsArray = [...words]; //now the set is an Array
+
+function quizNinja(){
+    const quiz = [
+        ["What is Superman's real name?","Clark Kent"],
+        ["What is Wonder Woman's real name?","Diana Prince"],
+        ["What is Batman's real name?","Bruce Wayne"]
+    ];
+
+    let score = 0 // initialize score
+    for(const [question, answer] of quiz)
+    {
+        const response = prompt(question);
+        if (response == answer){
+            score++;
+            alert("Correct!");
+        }
+        else
+        {
+            alert("Sorry, the correct answer was: " + answer)
+        }
+    }
+    //alert("Game Over \n You Scored " + score + " Point" + score === 1?".":"s.");
+    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+}
